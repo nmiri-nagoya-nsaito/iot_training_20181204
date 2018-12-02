a5145286897959e3f83429e92017ad64f19ead75
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2018-12-03 04:18:33 +0900

    不要なコードを削除するよう修正

--- ifttt_get.js
+++ ifttt_get.js
@@ -1,10 +1,10 @@
 var request = require("request");
 
-var IFTTT_MAKER_KEY="davb3w8mKPLqlagM8SGy0C";
+var EVENT_NAME="<イベント名>";
+var IFTTT_MAKER_KEY="<Webhookのキー>";
 
 // IFTTT のイベントを駆動
-//request('https://maker.ifttt.com/trigger/event_name/with/key/IFTTT_MAKER_KEY', function (error, response, body) {
-request('https://maker.ifttt.com/trigger/eve/with/key/davb3w8mKPLqlagM8SGy0C', function (error, response, body) { 
+request('https://maker.ifttt.com/trigger/EVENT_NAME/with/key/IFTTT_MAKER_KEY', function (error, response, body) {
         console.log('error:', error);
         console.log('statusCode:', response && response.statusCode);
         console.log('body:', body);
