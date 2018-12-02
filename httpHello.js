95cf4433f9e315fc55093e6f5c070ef5b886602f
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2018-12-03 04:36:36 +0900

    コメントの修正

--- httpHello.js
+++ httpHello.js
@@ -1,14 +1,14 @@
 // HTTPサーバ版　Hello World
 //
-// Webブラウザで http://<ラズベリーパイのIPアドレス>:3000/ に
-// アクセスするとHello World を表示する
-
+// Webブラウザで http://<ラズベリーパイのIPアドレス>:3000/ にアクセスすると
+// Hello World を表示する
 const http = require('http');
 
+// 3000番ポートで待ち受け．リクエストイベントが発生したらメッセージを表示する
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(3000);
 
+// 起動ログの表示
 console.log('Server running at http://127.0.0.1:3000/');
-
