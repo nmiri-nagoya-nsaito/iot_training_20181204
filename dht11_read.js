9019545c17c6d7f9bfc584528a1825f436b5141b
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2018-12-03 03:09:24 +0900

    DHT11読み出しサンプルのファイル名を変更

--- dht11_read.js
+++ dht11_read.js
@@ -1,6 +1,8 @@
+// node-dht-sensorモジュールの読み込み
+// (dht11_read.js)
 var sensor = require('node-dht-sensor');
 
-
+// センサーからの読み出し
 sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
